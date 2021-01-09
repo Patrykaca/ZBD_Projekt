ee1c70f1e738b08e1e5f7d7e2fbb114a9e3be18c
Patryk Kaca <patrykaca@gmail.com>
2021-01-09 23:37:01 +0100

    3

--- zapytania.sql
+++ zapytania.sql
@@ -36,8 +36,8 @@ go
 insert into pracownik (imie, nazwisko, pesel, telefon, data_zatrudnienia, stanowisko_id, lokal_id, dzial_id)
 values (N'Weles', N'Singh', '99010910460', '718107311', '2020/1/26 12:00:00', 3, 12, 7);
 
-select p.imie                                                       as imie,
-       p.nazwisko                                                   as nazwisko,
+select p.imie                                                        as imie,
+       p.nazwisko                                                    as nazwisko,
        'Wyslij sms na numer ' + p.telefon + N' aby złożyć życzenia!' as komunikat
 from pracownik p
 where substring(p.pesel, 3, 2) like '%' + cast(format(getdate(), 'MM') as varchar(2)) + '%'
@@ -49,3 +49,22 @@ from pracownik p
 where p.stanowisko_id = 3
    or p.stanowisko_id = 4
    or p.stanowisko_id = 5
+
+-- 3 --
+
+-- wypisuje dochód dla wykonanych zamówień
+with dane as (select (p1.cena * zp1.liczba_posilkow) as dochód,
+                     zm1.zamowienie_id
+              from posilek p1,
+                   zamowienie_posilek zp1,
+                   zamowienie zm1
+              where zm1.zamowienie_id = zp1.zamowienie_id
+                and zp1.posilek_id = p1.posilek_id)
+
+select d.zamowienie_id as zamówienie,
+       cast(sum(d.dochód) as numeric(10, 2)) as 'dochód przed opodatkowaniem',
+       cast(sum(d.dochód)*0.73 as numeric(10, 2)) as 'dochód po opodatkowaniu'
+from dane d, zamowienie zm
+where d.zamowienie_id = zm.zamowienie_id and
+      zm.status_zamowienia = 'Wykonane'
+group by d.zamowienie_id
